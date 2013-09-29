1d3e6bdc1da20a37de6703bd69b7d0303c33d2f2
Jorrit van Asselt @ MacBook Air <jbvansselt@gmail.com>
2013-09-29 20:37:41 +0200

    - merges files fine now. Extremely memory-inefficient.

--- TSFileMerger/AppDelegate.m
+++ TSFileMerger/AppDelegate.m
@@ -10,9 +10,75 @@
 
 @implementation AppDelegate
 
+
+#define kPath @"/Users/Jorrit/Desktop/iTunes Festival/Aloe Blacc/"
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [self mergeFiles];
+}
+
+- (void) mergeFiles{
+    NSArray * fileNames = [self fileList];
+    if (fileNames == nil) {
+        NSLog(@"Something went wrong");
+        return;
+    }
+    
+    NSInteger processedItems = 0;
+    CGFloat progression = 0.0f;
+    
+    NSString * currentFilePath = nil;
+    NSMutableData * mergedData = [[NSMutableData alloc] init];
+    for (NSString * aFileName in  fileNames) {
+        @autoreleasepool {
+            currentFilePath = [kPath stringByAppendingPathComponent: aFileName];
+            NSData * currentFile = [NSData dataWithContentsOfFile: currentFilePath];
+            [mergedData appendData: currentFile];
+            
+            processedItems++;
+            progression = (processedItems * 1.0f) / (fileNames.count * 1.0f);
+            
+            NSLog(@"Appended %@ - %2.2f", aFileName, (progression*100.0f));
+        }
+        
+    }
+    
+    NSError * error = nil;
+    NSLog(@"Now saving merged file...");
+    if (![mergedData writeToFile: [kPath stringByAppendingPathComponent: @"mergedFile.ts"]
+                         options: NSDataWritingAtomic
+                           error: &error]) {
+        NSLog(@"ERROR: could not write file: %@, %@", error, [error localizedDescription]);
+    } else{
+        NSLog(@"SUCCES!");
+    }
+}
+
+- (NSArray *) fileList{
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+    __block NSError * error = nil;
+    NSArray * contentOfDirectory = [fileManager contentsOfDirectoryAtPath: kPath error: &error];
+    if (contentOfDirectory == nil) {
+        NSLog(@"ERROR LIST CONTENT OF DIR:\n%@, %@", error, [error localizedDescription]);
+        return nil;
+    }
+    NSPredicate * containsDotTs = [NSPredicate predicateWithFormat: @"self CONTAINS %@", @".ts"];
+    NSArray * filteredArray = [contentOfDirectory filteredArrayUsingPredicate: containsDotTs];
+    
+    NSArray * sortedArray = [filteredArray sortedArrayUsingSelector: @selector(compare:)];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),^{
+        NSString * verifyList = [sortedArray componentsJoinedByString: @"\n"];
+        if (![verifyList writeToFile: [kPath stringByAppendingString: @"verifyList.txt"]
+                          atomically: YES
+                            encoding: NSUTF8StringEncoding
+                               error: &error]){
+            NSLog(@"ERROR: could not write verification list: %@, %@", error, [error localizedDescription]);
+        }
+    });
+    
+    return sortedArray;
 }
 
 @end
